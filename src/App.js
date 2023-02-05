24cb150e7814f1132050f51b7cdd0cf64a50fe1b
Alex Volkovich <volkovich@MacBook-Air-Aleksandr.local>
2023-02-06 01:22:19 +0300

    add keys

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import DrumPad from "./Components/DrumPad";
+import "./index.css";
+
+function App() {
+  const audioKeys = [
+    {
+      keyLetter: "Q",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyLetter: "W",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyLetter: "E",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyLetter: "A",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyLetter: "S",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyLetter: "D",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyLetter: "Z",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyLetter: "X",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyLetter: "C",
+      audioLink: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <div id="drum-machine">
+        <div id="display">
+          {audioKeys.map((audioKey) => (
+            <DrumPad
+              key={audioKey.keyLetter}
+              audioKey={audioKey.keyLetter}
+              sound={audioKey.keyLetter}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
